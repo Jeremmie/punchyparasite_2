3fc36935bb8f39772d2f94311ef161b9dffda20b
Jeremmie <61753395+Jeremmie@users.noreply.github.com>
2022-09-19 13:13:10 +0200

    suppression animation entre les pages
    en attendant une solution

--- modal.js
+++ modal.js
@@ -1,29 +1,29 @@
 var modals = document.getElementsByClassName("modal");
 
 for (let index = 0; index < modals.length; index++) {
-  var btn = document.getElementById("myBtn"+index);
-  let modal = document.getElementById("myModal"+index)
-  var span = document.getElementsByClassName("close")[index];
-  // When the user clicks the button, open the modal 
-  btn.onclick = function () {
-    var modal = document.getElementById("myModal"+index)
-    modal.style.display = "block";
-  }
+    var btn = document.getElementById("myBtn" + index);
+    let modal = document.getElementById("myModal" + index)
+    var span = document.getElementsByClassName("close")[index];
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        var modal = document.getElementById("myModal" + index)
+        modal.style.display = "block";
+    }
 
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function () {
-    // var modal = document.getElementById("myModal"+index)
-      modal.style.display = "none";
-  }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        // var modal = document.getElementById("myModal"+index)
+        modal.style.display = "none";
+    }
 };
 
 
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function (event) {
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
     for (let index = 0; index < modals.length; index++) {
-      var modal = document.getElementById("myModal"+index)
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
+        var modal = document.getElementById("myModal" + index)
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-  }
+}
--- script.js
+++ script.js
@@ -1,46 +1,95 @@
 
+var modals = document.getElementsByClassName("modal");
+
+for (let index = 0; index < modals.length; index++) {
+    var btn = document.getElementById("myBtn" + index);
+    let modal = document.getElementById("myModal" + index)
+    var span = document.getElementsByClassName("close")[index];
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        var modal = document.getElementById("myModal" + index)
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        // var modal = document.getElementById("myModal"+index)
+        modal.style.display = "none";
+    }
+};
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    for (let index = 0; index < modals.length; index++) {
+        var modal = document.getElementById("myModal" + index)
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+
 const wipe = document.querySelector('.wipe-transition');
 const allBandes = document.querySelectorAll('.bande');
 const TLAnim = new TimelineMax();
 
 function delay(n) {
-  return new Promise((done) => {
-    setTimeout(() => {
-      done();
-    }, n)
-  })
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n)
+    })
 }
+
+
 
 barba.init({
 
-  sync: true,
+    
 
-  transitions: [
-    {
-      async leave(){
+    sync: true,
 
-        const done = this.async();
+    transitions: [
+        {
+            
+            
+            async leave() {
+                
 
-        TLAnim
-        .to(allBandes, {height: '100%', stagger: 0.05})
+                const done = this.async();
 
-        // TLAnim.to(wipe, {left: '0%', ease: "power2.out", duration: 0.5});
+                TLAnim
+                    .to(allBandes, { height: '100%', stagger: 0.05 })
 
-        await delay(1500);
-        done();
+                // TLAnim.to(wipe, {left: '0%', ease: "power2.out", duration: 0.5});
 
-      },
-      enter(){
+                await delay(1500);
+                done();
+                
 
-        // TLAnim
-        // .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
-        // .set(wipe, {left: '-100%'})
+            },
 
-        TLAnim
-        .to(allBandes, {height: '0%', stagger: 0.05})
+            
 
-      }
-    }
-  ]
+            enter() {
+
+                // TLAnim
+                // .to(wipe, {left: '100%', ease:"power2.in", duration: 0.5})
+                // .set(wipe, {left: '-100%'})
+
+                TLAnim
+                    .to(allBandes, { height: '0%', stagger: 0.05 })
+                    
+            }
+            
+        }
+    ]
 
 })
+
+
+
+
+

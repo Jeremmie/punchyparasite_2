66b8c059139a5a4a778303ce10c13de5efd63e7d
Jeremmie <61753395+Jeremmie@users.noreply.github.com>
2022-09-21 16:17:00 +0200

    ajout autres plateformes home

--- butnhover.js
+++ butnhover.js
@@ -2,6 +2,8 @@ function btninit() {
     
     const btn = document.querySelector('.btn_test');
     const btn2 = document.querySelector('.btn_test2');
+    const btn3 = document.querySelector('.btn_test3');
+    const btn4 = document.querySelector('.btn_test4');
     
 
     btn.onmousemove = function (e) {
@@ -22,6 +24,21 @@ function btninit() {
         btn2.style.setProperty('--y', x + 'px');
     }
 
+    btn3.onmousemove = function (e) {
+        const x = e.pageX - btn3.offsetLeft;
+        const y = e.pageY - btn3.offsetTop;
+
+        btn3.style.setProperty('--x', x + 'px');
+        btn3.style.setProperty('--y', x + 'px');
+    }
+
+    btn4.onmousemove = function (e) {
+        const x = e.pageX - btn4.offsetLeft;
+        const y = e.pageY - btn4.offsetTop;
+
+        btn4.style.setProperty('--x', x + 'px');
+        btn4.style.setProperty('--y', x + 'px');
+    }
 };
 
 window.onload = btninit(); //rappel la fonction modalinit et la reload au changement de page
